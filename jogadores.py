cd28ed1e77cff53f57c58073403f3009b3a1b98a
Gabriel Bauer <gabbauer1218@gmail.com>
2024-04-15 15:43:37 -0300

    forca

---
+++ jogadores.py
@@ -0,0 +1,6 @@
+class Jogadores:
+    def __init__(self, nickname, score, derrotas, vitorias) -> None:
+        self.nickname = nickname
+        self.score = score
+        self.derrotas = derrotas
+        self.vitorias = vitorias
