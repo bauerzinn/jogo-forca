ec5134776c1d71bf036fcc786d44c86a2ac71e0e
Gabriel Bauer <gabbauer1218@gmail.com>
2024-04-15 18:52:20 -0300

    forca andamento
    Com alguns erros e bugs no if e/ou no while mas mais tarde termino...

--- menu.py
+++ menu.py
@@ -1,41 +1,108 @@
-# ideia: o Menu contem ou faz uma asoociação com Jogadores que são outra classe
+import random
 
-nomes = []
+def choose_word1():
+    with open("palavras1.txt", "r") as file:
+        words = file.readlines()
+    return random.choice(words).strip().lower()
 
-print('__BEM VINDO AO JOJO DA FORCA__')
+def choose_word2():
+    with open("palavras2.txt", "r") as file:
+        words = file.readlines()
+    return random.choice(words).strip().lower()
+
+def choose_word3():
+    with open("palavras3.txt", "r") as file:
+        words = file.readlines()
+    return random.choice(words).strip().lower()
+
+def display_word(word, guesses):
+    display = ""
+    for letter in word:
+        if letter in guesses:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+print('----------------------------------------------')
+print('__________BEM VINDO AO JOJO DA FORCA__________')
+print('----------------------------------------------')
 print()
+
 while True:
     nome = input('Digite seu nickname: ')
     print()
     
-    if nome not in nomes:
-        print(f'Olá {nome}, o jogo da forca se basea em uma palavra secreta e aleatória'
-            'você deve descobrir a palavra secreta chutando uma letra ou fazer um palpite de qual é a palavra secreta.')
-        print(f'A cada letra ou palpite errado um membro do jogador é exposto na forca'
-            '(cabeça, torso, pernas e braços) quando todos os membros estiverem na forca você perde.')
-        
-    else:
-        print(f'Bem vindo de volta {nome} seu histórico de partidas esta aqui:')
-        print('score')
-        print('hank')
-        
-    print()
-    opc = input('Começar jogo? s:sim n:não ')
+    tuto = input('Sabe como jogar? s:sim n:não ')
 
-    if opc == 's':
-        dificulade = input('Escolha a dificuldade: '
-               'fácil(1), difícil(2), extremo(3)')
-        if dificulade == '1':
-            print('jogo na dificulade 1')
-        elif dificulade == '2':
-            print('jogo na dificulade 2')
-        elif dificulade == '3':
-            print('jogo na dificulade 3')
+    if tuto == 's':
+        print()
+        print('certo...')
+        print()
+        
+        if  tuto == 'n':
+            print('_______Tutorial_______')
+            print(f'Olá {nome}, o jogo da forca se baseia em uma palavra')
+            print('secreta e aleatória. Você deve descobrir a palavra')
+            print('secreta chutando uma letra ou fazendo um palpite de qual')
+            print('é a palavra secreta.')
+            print()
+            print('A cada letra ou palpite errado, um membro do jogador é')
+            print('exposto na forca (cabeça, torso, pernas e braços). Quando')
+            print('todos os membros estiverem na forca, você perde.')
+
+            print()
+        opc = input('Começar jogo? s:sim n:não ')
+
+        if opc == 's':
+            dificuldade = input('Escolha a dificuldade: '
+                                'fácil(1), difícil(2), IMPOSSIVEL(3): ')
+            if dificuldade == '1':
+                print('Jogo na dificuldade 1')
+                word = choose_word1()
+            elif dificuldade == '2':
+                print('Jogo na dificuldade 2')
+                word = choose_word2()
+            elif dificuldade == '3':
+                print('Jogo na dificuldade 3')
+                word = choose_word3()
+            else:
+                print('Digite uma dificuldade válida.')
+
+            print('Começar jogo...')
+            
+            guessed_letters = []
+            attempts = 6
+
+            while attempts > 0:
+                print("\nPalavra: ", display_word(word, guessed_letters))
+                guess = input("Digite uma letra: ").lower()
+
+                if len(guess) != 1 or not guess.isalpha():
+                    print("Por favor, insira apenas uma letra válida.")
+                    continue
+
+                if guess in guessed_letters:
+                    print("Você já tentou esta letra. Tente outra.")
+                    continue
+
+                guessed_letters.append(guess)
+
+                if guess not in word:
+                    attempts -= 1
+                    print("Letra incorreta! Você tem {} tentativas restantes.".format(attempts))
+                else:
+                    print("Letra correta!")
+
+                if "_" not in display_word(word, guessed_letters):
+                    print("\nParabéns! Você adivinhou a palavra: ", word)
+                    break
+
+                if attempts == 0:
+                    print("\nVocê perdeu! A palavra era: ", word)
+            continue
+
+
         else:
-            print('digite uma dificuldade')
-        print('começar jojo')
-        continue
-    if opc == 'n':
-        continue
-    else:
-        print('digite a opção corretamente')
+            print('Digite a opção corretamente.')
+            break
