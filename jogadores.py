cb168a68ad75c207f3b1260327bfeb6da38d59be
Gabriel Bauer <gabbauer1218@gmail.com>
2024-04-25 23:00:38 -0300

    jogo-finalizado
    add sistemas de score com json e deixem mais bonito

--- jogadores.py
+++ jogadores.py
@@ -1,6 +1,76 @@
+
+import json
+
 class Jogadores:
-    def __init__(self, nickname, score, derrotas, vitorias) -> None:
+    def __init__(self, nickname, pontos, vitorias, derrotas):
         self.nickname = nickname
-        self.score = score
-        self.derrotas = derrotas
+        self.pontos = pontos
         self.vitorias = vitorias
+        self.derrotas = derrotas
+
+    def salva_nickname(self):
+        with open("jogadores.json", "r+") as arquivo:
+            jogadores = json.load(arquivo)
+        novo_jogador = {"nickname": self.nickname,
+                            "historico": {
+                                "pontuacao": self.pontos,
+                                "vitorias": self.vitorias,
+                                "derrotas": self.derrotas
+                                }
+                            }
+        jogadores.append(novo_jogador)
+        with open("jogadores.json", 'w') as arquivo:
+            json.dump(jogadores, arquivo, indent=4)
+    
+    def atualiza(self, pontos, vitorias, derrotas):
+        with open("jogadores.json", "r+") as arquivo:
+            jogadores = json.load(arquivo)
+            for jogador in jogadores:
+                if self.nickname == jogador['nickname']:
+                    jogador["historico"]["pontuacao"] += pontos
+                    jogador["historico"]["vitorias"] += vitorias
+                    jogador["historico"]["derrotas"] += derrotas
+                arquivo.seek(0)
+                json.dump(jogadores, arquivo, indent=4)
+                arquivo.truncate()
+
+    def cheka_nickname(self):
+        with open("jogadores.json", "r") as arquivo:
+            jogadores = json.load(arquivo)
+
+        for jogador in jogadores:
+            if jogador["nickname"] == self.nickname:
+                print("Bem vindo de volta jogador:", self.nickname)
+                print('HISTÓRICO_DE_PONTUAÇÃO')
+                print("Pontuação -> ", jogador["historico"]["pontuacao"])
+                print("Vitórias -> ", jogador["historico"]["vitorias"])
+                print("Derrotas -> ", jogador["historico"]["derrotas"])
+                return True
+        else:
+            return False
+        
+    def mostrar_info(self):
+        print('_Tutorial_')
+        print(f'Olá {self.nickname}, o jogo da forca se baseia em uma palavra')
+        print('secreta e aleatória. Você deve descobrir a palavra')
+        print('secreta chutando uma letra ou fazendo um palpite de qual')
+        print('é a palavra secreta.')
+        print()
+        print('A cada letra ou palpite errado, um membro do jogador é')
+        print('exposto na forca (cabeça, torso, pernas e braços). Quando')
+        print('todos os membros estiverem na forca, você perde.')
+
+class Ranking(Jogadores):
+    def _init_(self, nickname, pontos, vitorias, derrotas):
+        super()._init_(nickname, pontos, vitorias, derrotas)
+    
+    def ranking(self):
+        with open("jogadores.json", "r+") as arquivo:
+            jogadores = json.load(arquivo)
+        ranking = sorted(jogadores, key=lambda x: x["historico"]["pontuacao"], reverse=True)
+        print('RANKING_DOS_JOGADORES')
+        for i, jogador in enumerate(ranking, start=1):
+            print(f"{i}. {jogador['nickname']} - Pontuação: {jogador['historico']['pontuacao']} - Vitórias: {jogador['historico']['vitorias']}")
+     
+    #método pra criar posição no hanking a cada ultima jogada
+    #método para mostrar o hanking
--- menu.py
+++ menu.py
@@ -1,19 +1,38 @@
 import random
+from jogadores import Jogadores, Ranking
+
+desenhos_forca = [
+    "  __\n |    |\n |\n |\n |\n |\n|",
+    "  __\n |    |\n |    O\n |\n |\n |\n|",
+    "  __\n |    |\n |    O\n |    |\n |\n |\n|",
+    "  __\n |    |\n |    O\n |   /|\n |\n |\n|",
+    "  __\n |    |\n |    O\n |   /|\\\n |\n |\n|",
+    "  __\n |    |\n |    O\n |   /|\\\n |   /\n |\n|"
+]
 
 def choose_word1():
     with open("palavras1.txt", "r") as file:
-        words = file.readlines()
-    return random.choice(words).strip().lower()
+        lines = file.readlines()
+    word_info = random.choice(lines).strip().split(",")
+    word = word_info[0].lower()
+    hint = word_info[1].strip()
+    return word, hint
 
 def choose_word2():
     with open("palavras2.txt", "r") as file:
-        words = file.readlines()
-    return random.choice(words).strip().lower()
+        lines = file.readlines()
+    word_info = random.choice(lines).strip().split(",")
+    word = word_info[0].lower()
+    hint = word_info[1].strip()
+    return word, hint
 
 def choose_word3():
     with open("palavras3.txt", "r") as file:
-        words = file.readlines()
-    return random.choice(words).strip().lower()
+        lines = file.readlines()
+    word_info = random.choice(lines).strip().split(",")
+    word = word_info[0].lower()
+    hint = word_info[1].strip()
+    return word, hint
 
 def display_word(word, guesses):
     display = ""
@@ -24,77 +43,136 @@ def display_word(word, guesses):
             display += "_"
     return display
 
+def display_forca(erros):
+    print(desenhos_forca[erros])
+
 print('----------------------------------------------')
-print('__________BEM VINDO AO JOJO DA FORCA__________')
+print('BEM VINDO AO JOGO DA FORCA')
 print('----------------------------------------------')
 print()
 
 while True:
-    nome = input('Digite seu nickname: ')
+    nome = Jogadores(
+        input('Digite seu nickname: '), 0, 0, 0)
     print()
-
-    print('_______Tutorial_______')
-    print(f'Olá {nome}, o jogo da forca se baseia em uma palavra')
-    print('secreta e aleatória. Você deve descobrir a palavra')
-    print('secreta chutando uma letra ou fazendo um palpite de qual')
-    print('é a palavra secreta.')
-    print()
-    print('A cada letra ou palpite errado, um membro do jogador é')
-    print('exposto na forca (cabeça, torso, pernas e braços). Quando')
-    print('todos os membros estiverem na forca, você perde.')
+    if not nome.cheka_nickname():
+        nome.salva_nickname()
 
     print()
-    opc = input('Começar jogo? s:sim n:não ')
+    
+    ranking = Ranking(None, None, None, None)
+    ranking.ranking()
+
+
+    print()
+
+    nome.mostrar_info()
+
+    print()
+    opc = input('Começar jogo? (s:sim, n:não): ')
 
     if opc == 's':
-            dificuldade = input('Escolha a dificuldade: '
-                                'fácil(1), difícil(2), IMPOSSIVEL(3): ')
-            if dificuldade == '1':
-                print('Jogo na dificuldade 1')
-                word = choose_word1()
-            elif dificuldade == '2':
-                print('Jogo na dificuldade 2')
-                word = choose_word2()
-            elif dificuldade == '3':
-                print('Jogo na dificuldade 3')
-                word = choose_word3()
-            else:
-                print('Digite uma dificuldade válida.')
+        dificuldade = input('Escolha a dificuldade: fácil(1), difícil(2), IMPOSSIVEL(3): ')
+        if dificuldade == '1':
+            print('Jogo na dificuldade 1')
+            pontos_de_vitoria = 500
+            word = choose_word1()
+        elif dificuldade == '2':
+            pontos_de_vitoria = 1000
+            print('Jogo na dificuldade 2')
+            word = choose_word2()
+        elif dificuldade == '3':
+            pontos_de_vitoria = 2000
+            print('Jogo na dificuldade 3')
+            word = choose_word3()
+        else:
+            print('Digite uma dificuldade válida.')
 
-            print('Começar jogo...')
+        print('Começar jogo...')
+
+        if dificuldade == '1':
+            print('Jogo na dificuldade 1')
+            print('')
+            pontos_de_vitoria = 500
+            word, hint = choose_word1()
+            print("Dica:", hint)
+        elif dificuldade == '2':
+            pontos_de_vitoria = 1000
+            print('Jogo na dificuldade 2')
+            print('')
+            word, hint = choose_word2()
+            print("Dica:", hint)
+        elif dificuldade == '3':
+            pontos_de_vitoria = 2000
+            print('Jogo na dificuldade 3')
+            print('')
+            word, hint = choose_word3()
+            print("Dica:", hint)
+        
+        guessed_letters = []
+        attempts = 6
+        diminuidor_pontos = 0
+        while attempts > 0:
+
+            print("\nPalavra: ", display_word(word, guessed_letters))
+            display_forca(6 - attempts)
+            guess = input("Digite uma letra: ").lower()
             
-            guessed_letters = []
-            attempts = 6
+            if len(guess) != 1 or not guess.isalpha():
+                print("Por favor, insira apenas uma letra válida.")
+                continue
 
-            while attempts > 0:
-                print("\nPalavra: ", display_word(word, guessed_letters))
-                guess = input("Digite uma letra: ").lower()
+            if guess in guessed_letters:
+                print("Você já tentou esta letra. Tente outra.")
+                continue
 
-                if len(guess) != 1 or not guess.isalpha():
-                    print("Por favor, insira apenas uma letra válida.")
-                    continue
+            guessed_letters.append(guess)
 
-                if guess in guessed_letters:
-                    print("Você já tentou esta letra. Tente outra.")
-                    continue
+            if guess not in word:
+                attempts -= 1
+                print("Letra incorreta! Você tem {} tentativas restantes.".format(attempts))
+                diminuidor_pontos += 100
+            else:
+                print("Letra correta!")
 
-                guessed_letters.append(guess)
+            if "_" not in display_word(word, guessed_letters):
+                print("\nParabéns! Você adivinhou a palavra: ", word)
+                vitorias = 1
+                while True:
+                    opcao = input('DESEJA SALVAR O JOGO? (s:sim/n:não)')
+                    if opcao == 's':
+                        pontos_de_vitoria = pontos_de_vitoria - diminuidor_pontos
+                        nome.atualiza(pontos_de_vitoria, vitorias, 0)
+                        break
+                    elif opcao == 'n':
+                        print('jogo não foi salvo')
+                        break
+                    else:
+                        print('digite corretamente')
+                        continue
+                break
 
-                if guess not in word:
-                    attempts -= 1
-                    print("Letra incorreta! Você tem {} tentativas restantes.".format(attempts))
-                else:
-                    print("Letra correta!")
+            if attempts == 0:
+                print("\nVocê perdeu! A palavra era: ", word)
+                derrotas = 1
+                while True:
+                    opcao = input('DESEJA SALVAR O JOGO? (s:sim/n:não)')
+                    if opcao == 's':
+                        nome.atualiza(0, 0, derrotas)
+                        break
+                    elif opcao == 'n':
+                        print('jogo não foi salvo')
+                        break
+                    else:
+                        print('digite corretamente')
+                        continue
+                break
+        continue
 
-                if "_" not in display_word(word, guessed_letters):
-                    print("\nParabéns! Você adivinhou a palavra: ", word)
-                    break
-
-                if attempts == 0:
-                    print("\nVocê perdeu! A palavra era: ", word)
-            continue
-
+    elif opc == 'n':
+        print('Jogo encerrado.')
+        break
 
     else:
-        print('Digite a opção corretamente.')
-        break
+        print('Digite uma opção válida.')
+        continue
